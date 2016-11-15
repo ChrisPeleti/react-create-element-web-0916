cafc094fa437f477e75e79b46e4b87797e86388c
ChrisPeleti <chrisyred@gmail.com>
2016-11-15 16:31:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const list =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'My favorite ice cream flavors'),
-    React.createElement('ul', {},
-      [
-        React.createElement('li', { className: 'brown' }, 'Chocolate'),
-        React.createElement('li', { className: 'white' }, 'Vanilla'),
-        React.createElement('li', { className: 'yellow' }, 'Banana')
-      ]
-    ));
-const title = React.createElement('h1', {}, 'My First React Code')
-const paragraph =  React.createElement('p', {}, 'Writing some more HTML. Cool Stuff!')
-const container = React.createElement('div', {}, [title, paragraph])
+// const list =
+//   React.createElement('div', {},
+//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
+//     React.createElement('ul', {},
+//       [
+//         React.createElement('li', { className: 'brown' }, 'Chocolate'),
+//         React.createElement('li', { className: 'white' }, 'Vanilla'),
+//         React.createElement('li', { className: 'yellow' }, 'Banana')
+//       ]
+//     ));
+// const title = React.createElement('h1', {}, 'My First React Code')
+// const paragraph =  React.createElement('p', {}, 'Writing some more HTML. Cool Stuff!')
+// const container = React.createElement('div', {}, [title, paragraph])
+// ReactDOM.render(
+//   list,
+//    document.getElementById('main')
+//  );
+var nameLabel = React.createElement('label', {for: ""}, "Name:")
+var homeLabel = React.createElement('label', {for: ""}, "Hometown:")
+var nameInput = React.createElement('input', {type: "text", name: "user[name]", value: ""})
+var homeInput = React.createElement('input', {type: "text", name: "user[hometown]", value: ""})
+var subInput = React.createElement('input', {type: "submit", name: "name", value: "Create User"})
+var form = React.createElement('form', {className: "users", action: "/users", method: "post"}, [nameLabel, nameInput, homeLabel, homeInput, subInput])
+var div = React.createElement('div', {className: "KEK", id: "idLUL"}, form)
+
+
 ReactDOM.render(
-  list,
-   document.getElementById('main')
- );
+  div,
+  document.getElementById('main')
+)
+
+  // ReactDOM.render(
+  //   button,
+  //   document.getElementById('main')
+  // )
